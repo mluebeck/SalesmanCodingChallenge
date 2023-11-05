e01a4d7b2bd5475251f6d97e18806987bc6796ae
mluebeck <mariorotz@googlemail.com>
2023-11-05 11:34:10 +0100

    adding xcode project

---
+++ Salesman/Salesman/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Salesman
+//
+//  Created by Mario Rotz on 05.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Salesman/Salesman/SalesmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesmanApp.swift
+//  Salesman
+//
+//  Created by Mario Rotz on 05.11.23.
+//
+
+import SwiftUI
+
+@main
+struct SalesmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
